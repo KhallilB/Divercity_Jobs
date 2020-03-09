22229b98ea6da11174ed2345523481841b7c2287
KhallilB <khallil.bailey@gmail.com>
2020-03-09 12:55:30 -0700

    [add] auth components

---
+++ src/components/Auth/SignUp/SignUp.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Signup.css';
+
+class SignUp extends React.Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Signup Works</h1>
+            </div>
+        )
+    }
+}
+
+export default SignUp;

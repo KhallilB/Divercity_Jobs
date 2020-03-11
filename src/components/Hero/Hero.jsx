78082431d0f9c695186bf0cf97473d8d45b7a0f1
KhallilB <khallil.bailey@gmail.com>
2020-03-11 06:43:53 -0700

    [add] spice up home

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Hero.css';
 
 import backgroundImage from '../../assets/diversity-background.png';
@@ -15,6 +16,13 @@ class Hero extends React.Component {
                 <div className="hero__background">
                     <img src={backgroundImage} alt="#" />
                     <div className="hero__transparent" />
+
+                    <div className="hero__welcome_text">
+                        <h1>Welcome to Divercity Jobs</h1>
+                        <h3>The World’s Largest Minority Professional Network</h3>
+
+                        <Link to="/jobs"><button>See Jobs</button></Link>
+                    </div>
                 </div>
             </div>
         )
